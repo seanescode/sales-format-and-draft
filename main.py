1fb369f5194739cdaac02f8bc1636eb62b04efe6
Sean Redmond <116457224+seanescode@users.noreply.github.com>
2026-08-11 23:21:02 +0100

    Move automation logic into main function

--- main.py
+++ main.py
@@ -8,86 +8,90 @@ from logger import get_logger
 from modules.workbook import format_excel
 from modules.outlook import create_outlook_email
 
-logger = get_logger()
+def main():
+    logger = get_logger()
 
-# find the absolute, full folder path to the exact script file (main.py) that is running right now
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-config_file = os.path.join(SCRIPT_DIR, "config.ini")
+    # find the absolute, full folder path to the exact script file (main.py) that is running right now
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+    config_file = os.path.join(SCRIPT_DIR, "config.ini")
 
-config = configparser.ConfigParser()
-config.read(config_file)
-
-
-# Extract data values from the INI file (for email)
-recipients = config.get("EMAIL_SETTINGS", "recipients")
-cc_recipients = config.get("EMAIL_SETTINGS", "cc_recipients")
-email_subject = config.get("EMAIL_SETTINGS", "subject")
-email_body = config.get("EMAIL_CONTENT", "body")
+    config = configparser.ConfigParser()
+    config.read(config_file)
 
 
-# Clean up data structures for your email_tools function requirements
-recipient_list = [email.strip() for email in recipients.split(";")]
-cc_recipient_list = [email.strip() for email in cc_recipients.split(";")]
+    # Extract data values from the INI file (for email)
+    recipients = config.get(section="EMAIL_SETTINGS", option="recipients")
+    cc_recipients = config.get(section="EMAIL_SETTINGS", option="cc_recipients")
+    email_subject = config.get(section="EMAIL_SETTINGS", option="subject")
+    email_body = config.get(section="EMAIL_CONTENT", option="body")
 
 
-# Extract data values from the INI file (for Excel formatting)
-bold_title = config.getboolean("EXCEL_REPORT_FORMATTING", "bold_header")
-italic_header = config.getboolean("EXCEL_REPORT_FORMATTING", "italic_font")
-header_colour = ast.literal_eval(
-    config.get("EXCEL_REPORT_FORMATTING", "heading_colour")
-)
-
-excel_sheet_name = config.get("EXCEL_REPORT_FORMATTING", "sheet_name")
-attachment_paths = config.get("EXCEL_REPORT_FORMATTING", "attachment_paths").split(";")
-attachment_paths = [file.strip() for file in attachment_paths]
-
-app = None
-wb = None
+    # Clean up data structures for your email_tools function requirements
+    recipient_list = [email.strip() for email in recipients.split(";")]
+    cc_recipient_list = [email.strip() for email in cc_recipients.split(";")]
 
 
-try:
-    logger.info("Starting sales report automation")
-
-    app = xlwings.App(visible=False)
-    wb = app.books.open(attachment_paths[0])
-
-    logger.info("Excel workbook opened successfully")
-
-    ws = wb.sheets[excel_sheet_name]
-
-    format_excel(
-        wb,
-        ws,
-        header_colour,
-        bold_title,
-        italic_header
+    # Extract data values from the INI file (for Excel formatting)
+    bold_title = config.getboolean(section="EXCEL_REPORT_FORMATTING", option="bold_header")
+    italic_header = config.getboolean(section="EXCEL_REPORT_FORMATTING", option="italic_font")
+    header_colour = ast.literal_eval(
+        config.get(section="EXCEL_REPORT_FORMATTING", option="heading_colour")
     )
 
-    logger.info("Excel formatting completed successfully")
+    excel_sheet_name = config.get(section="EXCEL_REPORT_FORMATTING", option="sheet_name")
+    attachment_paths = config.get(section="EXCEL_REPORT_FORMATTING", option="attachment_paths").split(";")
+    attachment_paths = [file.strip() for file in attachment_paths]
 
-    create_outlook_email(
-        recipients=recipient_list,
-        cc_recipients=cc_recipient_list,
-        subject=email_subject,
-        body=email_body,
-        attachments=attachment_paths
-    )
-
-    logger.info("Outlook email draft created successfully")
+    app = None
+    wb = None
 
 
-except Exception as e:
-    logger.exception(f"Automation failed: {e}")
-    raise
+    try:
+        logger.info("Starting sales report automation")
+
+        app = xlwings.App(visible=False)
+        wb = app.books.open(attachment_paths[0])
+
+        logger.info("Excel workbook opened successfully")
+
+        ws = wb.sheets[excel_sheet_name]
+
+        format_excel(
+            wb,
+            ws,
+            header_colour,
+            bold_title,
+            italic_header
+        )
+
+        logger.info("Excel formatting completed successfully")
+
+        create_outlook_email(
+            recipients=recipient_list,
+            cc_recipients=cc_recipient_list,
+            subject=email_subject,
+            body=email_body,
+            attachments=attachment_paths
+        )
+
+        logger.info("Outlook email draft created successfully")
 
 
-finally:
-    if wb is not None:
-        wb.close()
-        logger.info("Workbook closed")
+    except Exception as e:
+        logger.exception(f"Automation failed: {e}")
+        raise
 
-    if app is not None:
-        app.quit()
-        logger.info("Excel application closed")
 
-    logger.info("Automation finished")
+    finally:
+        if wb is not None:
+            wb.close()
+            logger.info("Workbook closed")
+
+        if app is not None:
+            app.quit()
+            logger.info("Excel application closed")
+
+        logger.info("Automation finished")
+
+if __name__ == "__main__":
+    main()
